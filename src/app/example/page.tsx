1cf6659981158be71ec7bea750b019159ab4b6c6
Vitalii <v.hulaievych@gmail.com>
2026-04-03 17:38:45 +0100

    delete template

--- src/app/example/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import Dashboard from "@/__template/dashboard/Dashboard";
-import MarketingPage from "@/__template/marketing-page/MarketingPage";
-
-export default function StartPage() {
-  return <MarketingPage />;
-}
