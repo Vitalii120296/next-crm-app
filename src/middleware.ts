cdddd1d8807f03ce58ffccc6a67918dbedd049ff
Vitalii <v.hulaievych@gmail.com>
2026-04-03 17:55:29 +0100

    delete template

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get("token");
+  const token = req.cookies.get("access_token");
 
   if (!token) {
     return NextResponse.redirect(new URL("/sign-in", req.url));
