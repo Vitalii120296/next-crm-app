a9a5350179f4dc1c0cc8cefd87fd020ac736d8a2
Vitalii <v.hulaievych@gmail.com>
2026-04-09 14:38:24 +0100

    deleted unused files

--- src/types/product.ts
+++ src/types/product.ts
@@ -3,10 +3,11 @@ import { Client } from "./client";
 export type Product = {
   id: string;
   name: string;
-  description: string;
-  price: string;
-  sku: string;
-  createdAt: Date;
-  clients: Pick<Client, "id" | "first_name" | "last_name" | "status">[];
-  file?: File;
+  description?: string;
+  price?: string;
+  sku?: number;
+  clients?: Pick<Client, "id" | "name" | "surname" | "status">[];
+
+  userId?: string;
+  createdAt?: Date;
 };
